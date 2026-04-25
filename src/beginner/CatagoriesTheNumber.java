56976c31b83af376116bc2f1ac9868f024ccf8ea
Sangram <180935703+sangramofc@users.noreply.github.com>
2026-04-25 23:41:36 +0530

    first program

--- src/beginner/CatagoriesTheNumber.java
+++ src/beginner/CatagoriesTheNumber.java
@@ -1,7 +1,14 @@
 package beginner;
 
+import java.util.Scanner;
+
 public class CatagoriesTheNumber {
     static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int x = sc.nextInt();
+
+        System.out.println((x > 0) ? 1 : (x < 0) ? -1 : 0);
 
     }
 }
